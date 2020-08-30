1284671f0e9f0e2e001309165ef438e0a3685320
Velina Petrova <velinarp@gmail.com>
2020-08-31 01:28:54 +0800

    adding initial tailwind styles

---
+++ assets/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    // Some useful comment
+    extend: {
+      fontFamily: {
+        'pacifico': ['Pacifico', 'cursive']
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
