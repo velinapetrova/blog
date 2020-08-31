5be51dd5a821c2234fc12e27ecbb89419ea8a69d
Velina Petrova <velinarp@gmail.com>
2020-08-31 13:49:48 +0800

    task: modify header styles, logo and icon
    Signed-off-by: Velina Petrova <velinarp@gmail.com>

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    // Some useful comment
     extend: {
       fontFamily: {
-        'pacifico': ['Pacifico', 'cursive']
+        'megrim': ['Megrim', 'cursive'],
+        'roboto-cond': ['Roboto Condensed','Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif']
       },
+      colors: {
+        'light-gray': '#d6d2bc',
+        'dark-gray': '#252323',
+        'darker-gray': '#272727'
+      }
     },
   },
   variants: {},
